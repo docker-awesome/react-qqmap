fa00d98ffff3dd62b82fc6069bda9fac0e9d5371
soga <docker.cpu@qq.com>
2023-04-24 17:22:55 +0800

    feat(CI): deploy to github pages

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/react-qqmap/',
+  publicPath: '/react-qqmap/',
   themeConfig: {
     name: 'Qmap',
   },
